dfb213d6daa9cab5e7a22d45f1bdbe1825b5d5dc
Davids-Adamovics <davidsadamovicss@gmail.com>
2025-12-13 14:02:21 +0200

    EDIT: redid skillscontroller index function

--- backend/app/Http/Controllers/Api/SkillsController.php
+++ backend/app/Http/Controllers/Api/SkillsController.php
@@ -7,14 +7,20 @@ use App\Models\Skill;
 use App\Http\Requests\StoreSkillRequest;
 use App\Http\Requests\UpdateSkillRequest;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
 
 class SkillsController extends Controller
 {
-    public function index()
-    {
-        return Cache::remember('skills_list', 60, function() {
-            return Skill::all();
+    public function index(Request $request){
+        $page = $request->query('page', 1);
+        $cacheKey = "skills_page_" . $page;
+
+        return Cache::remember($cacheKey, 60, function() {
+            return Skill::paginate(6);
         });
+        // return Cache::remember('skills_list', 60, function() {
+        //     return Skill::all();
+        // });
     }
 
     public function store(StoreSkillRequest $request)
